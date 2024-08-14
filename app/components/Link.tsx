ab1d9864418c1a8fbd3585eb436e15734a9c5c7b
Daivik Dhar <165635276+dhardaiv@users.noreply.github.com>
2024-08-14 17:45:10 -0400

    change colour

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -7,7 +7,7 @@ interface LinkProps {
 
 const Link: React.FC<LinkProps> = ({ href, children }) => {
   return (
-    <a href={href} target="_blank" className="text-zinc-500 underline decoration-1 underline-offset-2 decoration-dashed hover:text-orange-200 transition duration-300">
+    <a href={href} target="_blank" className="text-zinc-500 underline decoration-1 underline-offset-2 decoration-dashed hover:text-blue-300 transition duration-300">
       {children}
     </a>
   );
