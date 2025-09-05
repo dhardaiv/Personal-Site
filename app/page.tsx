3b0ce987a70c727200eb69926da8c0faded45ec6
Daivik Dhar <165635276+dhardaiv@users.noreply.github.com>
2025-09-04 20:13:17 -0400

    update semester

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
               hey, i am <span className="text-blue-300">daivik.</span> 
             </p>
             <p>i am a {age} year old software engineer from Toronto, 🇨🇦 interested in all things programming.</p>
-            <p>currently, i am tinkering with unsupervised and reinforcement learning algorithms, expanding on my frontend development skills and getting ready for my <a href="https://chem-eng.utoronto.ca/"><span className="text-blue-300">2F term in ce @ uoft</span></a>.</p>         
-            <p>feel free to shoot me an email at <a href="mailto:vikdai217@gmail.com"><span className="text-blue-300">daivikdhar10@gmail.com</span></a>.</p>
+            <p>currently, i am tinkering with unsupervised and reinforcement learning algorithms, expanding on my frontend development skills and getting ready for my <a href="https://chem-eng.utoronto.ca/"><span className="text-blue-300">3F term in ce @ uoft</span></a>.</p>         
+            <p>feel free to shoot me an email at <a href="mailto:daivikdhar10@gmail.com"><span className="text-blue-300">daivikdhar10@gmail.com</span></a>.</p>
             
           </div>
         </motion.div>
